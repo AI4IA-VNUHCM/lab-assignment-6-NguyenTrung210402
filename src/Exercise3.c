6f5b2877613a09afc056de18a022368e26c3c77c
NguyenTrung210402 <91875395+NguyenTrung210402@users.noreply.github.com>
2021-11-22 16:42:27 +0700

    edit

--- src/Exercise3.c
+++ src/Exercise3.c
@@ -9,51 +9,52 @@ Ex:
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 #include <string.h>
 #include <ctype.h>
 
-int main()
-{
-    char input[100] = "";
-    char word[20] = "";
-    char longest[20] = "";
-    char shortest[20] = "";
-
-    int wordindex = 0;
-    printf ("Input a string: ");
-    fgets(input, sizeof(input), stdin);
-
-    for (int inputindex = 0; inputindex < strlen(input); inputindex++){
-        while (inputindex < strlen(input)&&
-                !isspace(input[inputindex])&&
-                isalnum(input[inputindex])) {
-                    word[wordindex++]=input[inputindex++];
-                }
-                if (wordindex != 0)
+void Ex3(char *str){
+    //Your codes here
+        char word[20]; 
+        char l[20], s[20], c;
+        int i=0, j=0, tie=0;
+        str[i - 1] = '\0';
+        for (i = 0; i < strlen(str); i++)
+        {
+            while (i < strlen(str) && !isspace(str[i]) && isalnum(str[i]))
+            {
+                word[j++] = str[i++];
+            }
+            if (j != 0)
+            {
+                word[j] = '\0';
+                if (!tie)
                 {
-                    word[wordindex] = '\0';
-
-                    if (strlen(longest) == 0)
-                    {
-                        strcpy(longest, word);
-                    }
-                    if (strlen(shortest) == 0)
-                    {
-                        strcpy(shortest, word);
-                    }
-                    if (strlen(word) > strlen(longest))
-                    {
-                        strcpy(longest, word);
-                    }
-                    if (strlen(word) < strlen(shortest))
-                    {
-                        strcpy(shortest, word);
-                    }
-                    wordindex = 0;
+                    tie = !tie;
+                    strcpy(l, word);
+                    strcpy(s, word);
                 }
-    }
-    printf ("Longest word: '%s'", longest);
-    printf ("\nShortest word: '%s'", shortest);
+                if (strlen(word) > strlen(l))
+                {
+                    strcpy(l, word);
+                }
+                if (strlen(word) < strlen(s))
+                {
+                    strcpy(s, word);
+                }
+                j = 0;
+            }
+        }
+        printf("Shortest word: %s\n", s);
+        printf("Longest word: %s",l);
+}
+
+int main(int argc, char *argv[]) {
+    //testing variable, applying it to your algorithm for auto-evaluating
+    char *testcase = argv[1];
+
+    Ex3(testcase);
 
     return 0;
 }
