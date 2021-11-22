7b75d6ab7f9ec0a65870c846a2fcd96447cffc46
NguyenTrung210402 <91875395+NguyenTrung210402@users.noreply.github.com>
2021-11-22 15:30:41 +0700

    2

--- src/Exercise3.c
+++ src/Exercise3.c
@@ -9,20 +9,51 @@ Ex:
 */
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
 #include <string.h>
+#include <ctype.h>
 
-void Ex3(char *str){
-	//Your codes here
-	
-}
+int main()
+{
+    char input[100] = "";
+    char word[20] = "";
+    char longest[20] = "";
+    char shortest[20] = "";
 
-int main(int argc, char *argv[]) {
-	//testing variable, applying it to your algorithm for auto-evaluating
-	char *testcase = argv[1];
-	
-	Ex3(testcase);
-	
-	return 0;
+    int wordindex = 0;
+    printf ("Input a string: ");
+    fgets(input, sizeof(input), stdin);
+
+    for (int inputindex = 0; inputindex < strlen(input); inputindex++){
+        while (inputindex < strlen(input)&&
+                !isspace(input[inputindex])&&
+                isalnum(input[inputindex])) {
+                    word[wordindex++]=input[inputindex++];
+                }
+                if (wordindex != 0)
+                {
+                    word[wordindex] = '\0';
+
+                    if (strlen(longest) == 0)
+                    {
+                        strcpy(longest, word);
+                    }
+                    if (strlen(shortest) == 0)
+                    {
+                        strcpy(shortest, word);
+                    }
+                    if (strlen(word) > strlen(longest))
+                    {
+                        strcpy(longest, word);
+                    }
+                    if (strlen(word) < strlen(shortest))
+                    {
+                        strcpy(shortest, word);
+                    }
+                    wordindex = 0;
+                }
+    }
+    printf ("Longest word: '%s'", longest);
+    printf ("\nShortest word: '%s'", shortest);
+
+    return 0;
 }
