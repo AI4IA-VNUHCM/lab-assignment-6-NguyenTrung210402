0b3cb1211f663bf8512360dbd1114616e62f13e6
NguyenTrung210402 <91875395+NguyenTrung210402@users.noreply.github.com>
2021-11-22 14:45:24 +0700

    edit

--- src/Exercise1.c
+++ src/Exercise1.c
@@ -12,7 +12,19 @@
 
 void Ex1(int n){
 	//Your codes here
-	
+	int sum,digit1,digit2,digit3,digit4;
+    printf("enter four digit integer:\n");
+    scanf("%d", &digit1,&digit2,&digit3,&digit4);
+
+    digit1=(digit1%1000)/10;
+    digit2=(digit2%1000)/10;
+    digit3=(digit3%1000)/10;
+    digit4=(digit4%1000)/10;
+
+    /*calculation*/
+    sum=digit1+digit2+digit3+digit4;
+
+    printf ("answer is: %d\n",sum);
 }
 
 int main(int argc, char *argv[]) {
